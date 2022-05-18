f74b68212fd61153dbf24a5b430804e43eb0573e
Brunda Katragadda <brundakatragadda17@gmail.com>
2022-05-18 13:21:31 +0530

    jobcard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { JobcardComponent } from './jobcard/jobcard.component';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,9 @@ import { SignupComponent } from './signup/signup.component';
     NavbarComponent,
     HomeComponent,
     WishlistComponent,
-    
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    JobcardComponent
   ],
   imports: [
     BrowserModule,
